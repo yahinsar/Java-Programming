d2c6d330e1a6dffd197004703d1c07eba83faf69
Shamil Yahin <yahinshamil02@gmail.com>
2024-02-14 00:10:10 +0400

    feat: add user input in main

--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -1,7 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.print("Введите пункт первого задания (A, B, C) или E для остановки программы: ");
+            String userInput = scanner.next();
+            switch (userInput.toUpperCase()) {
+                case "A":
+                    new A().printHelloWorld();
+                    break;
+                case "B":
+                    new B().calculation();
+                    break;
+                case "C":
+                    new C().readFromFile("input.txt");
+                    break;
+                case "E":
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Неверный ввод");
+            }
+        }
     }
 }
