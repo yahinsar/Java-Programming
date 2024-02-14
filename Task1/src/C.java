cc7b5ddf9c2f9bc53fb95e1ecce7d9927de80cd5
Shamil Yahin <yahinshamil02@gmail.com>
2024-02-14 20:06:48 +0400

    bugfix: edit c class

--- Task1/src/C.java
+++ Task1/src/C.java
@@ -5,11 +5,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class C {
-    public static void printInitials(String[] fio) {
+    public void printInitials(String[] fio) {
         System.out.printf("%s %c. %c.\n", fio[1], fio[0].charAt(0), fio[2].charAt(0));
     }
 
-    public static boolean isGoodFIO(String[] fio) {
+    public boolean isGoodFIO(String[] fio) {
         boolean isGood = true;
         Pattern fioPattern = Pattern.compile("^[А-ЯЁA-Z][а-яёa-z]*$");
         for (String name : fio) {
@@ -22,16 +22,18 @@ public class C {
         try {
             File file = new File(inputFilename);
             Scanner in = new Scanner(file);
-            String[] fio = new String[3];
-            for (int i = 0; i < fio.length; i++)
-                if (in.hasNext())
-                    fio[i] = in.next();
-                else
-                    throw new Exception("В файле не хватает данных");
-            if (isGoodFIO(fio))
-                printInitials(fio);
-            else
-                throw new Exception("В ФИО есть недопустимые символы, идите в ЗАГС");
+            while(in.hasNextLine()) {
+                String[] fio = new String[3];
+                for (int i = 0; i < fio.length; i++)
+                    if (in.hasNext())
+                        fio[i] = in.next();
+                    else
+                        continue;
+                if (isGoodFIO(fio))
+                    printInitials(fio);
+                if (in.hasNextLine())
+                    in.nextLine();
+            }
             in.close();
         } catch (FileNotFoundException e) {
             System.out.println("Не удается открыть файл: " + e.getMessage());
