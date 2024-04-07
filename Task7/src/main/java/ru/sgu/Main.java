a3afd8bac605527e93e84c1ed9bbbd5413bd3cb1
Shamil Yahin <yahinshamil02@gmail.com>
2024-04-07 20:55:41 +0400

    feat: create Task7

---
+++ Task7/src/main/java/ru/sgu/Main.java
@@ -0,0 +1,10 @@
+package main.java.ru.sgu;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("аче)");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
